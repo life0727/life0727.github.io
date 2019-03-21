af6827c714fb94b8edc0c237720d26bbab553327
life0727 <mmlife0727@gmail.com>
2019-03-21 19:03:24 +0800

    Site updated: 2019-03-21 19:03:22

--- js/src/affix.js
+++ js/src/affix.js
@@ -5,8 +5,6 @@
  * Copyright 2011-2015 Twitter, Inc.
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
-
-
 +function ($) {
   'use strict';
 
