3839c1648424b660d3e1d169f25dbefcc6c7f68e
life0727 <mmlife0727@gmail.com>
2019-03-21 16:17:51 +0800

    Site updated: 2019-03-21 16:17:50

--- wall/index.js
+++ wall/index.js
@@ -12,7 +12,7 @@ async function getWallData(){
     let WallDataArr = [];
     YG.on('child_added', function(datasnapshot) {
         let obj = Object.assign({},datasnapshot.val());
-        obj.time = new Date(datasnapshot.val().time).Format("yyyy/MM/dd hh:mm")
+        obj.time = JSON.parse(datasnapshot.val().time.replace(/T/g," ")).slice(0,16)
         WallDataArr.push(obj)
         console.log(WallDataArr)
     });
@@ -24,18 +24,33 @@ var app = new Vue({
     data: {
         data:[]
     },
-    mounted() {
-       
+    created() {
         getWallData()
-                    .then(data => this.data = data)
+                    .then(data => {
+                        this.data = data
+                    })
+    },
+    mounted() {    
+                   
+    },
+    updated() {
+        let boxList = this.$refs.box;
+        var colorList = ['#f34c81','#ffe75f','#688dff','#855df3','#23aacf','#ff6f7a','#60b680','#fd9c35','#dd4444'];
+        console.log(boxList)
+        boxList.forEach(element => {
+            element.style.boxShadow = '5px 5px 2px #ccc'
+            element.style.background = colorList[parseInt(Math.random()*8)]
+        });
     },
     methods: {
-
+       
     },
     // watch:{
-    //     toChildMsg:{
+    //     data:{
     //         handler(val){
-    //             console.log(val)
+    //             this.$nextTick(() => {
+    //                 console.log(this.$refs.box)[0] 
+    //             })
     //         },
     //         deep:true
     //     }
